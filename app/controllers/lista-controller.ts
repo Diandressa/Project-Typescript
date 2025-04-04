bf6a203680e9c54fc199f4ba2b925dc6d768e644
Andressa <andressa_nicolau@hotmail.com>
2025-04-04 12:05:07 -0300

    iniciou desenvolvimento de array para listas

--- app/controllers/lista-controller.ts
+++ app/controllers/lista-controller.ts
@@ -1,9 +1,11 @@
 import { Lista } from "../models/lista.js";
+import { Listas } from "../models/listas.js";
 
 export class ListaController {
     dataInput:HTMLInputElement
     nomeInput:HTMLInputElement
     horaInput:HTMLInputElement
+    private listasArray = new Listas;
 
     constructor(){
         this.dataInput = document.querySelector("#data") as HTMLInputElement;
@@ -17,7 +19,11 @@ export class ListaController {
         console.log(this.horaInput.value)
         console.log(this.nomeInput.value)
 
+        this.listasArray.adiciona()
+
         this.requiredForm(this.nomeInput.value)
+
+        return new Lista(data, this.horaInput.value,this.nomeInput.value)
     }
 
     requiredForm(nome: string):void{
---
+++ app/models/listas.ts
@@ -0,0 +1,11 @@
+import { Lista } from "./lista";
+
+export class Listas {
+    private listas:Lista[] = [];
+
+    adiciona(listas:Lista){
+        console.log("adicionou")
+    }
+}
+
+//const listasArray = new Listas();
