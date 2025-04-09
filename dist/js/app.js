47fc6a1d7b9033de450d6e69052c93922c939ac3
Andressa <andressa_nicolau@hotmail.com>
2025-04-09 17:41:40 -0300

    limpou js

--- dist/js/app.js
+++
@@ -1,7 +0,0 @@
-import { ListaController } from "./controllers/lista-controller.js";
-const controller = new ListaController();
-const form = document.querySelector('form');
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    controller.criarLista();
-});
